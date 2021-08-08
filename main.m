1c2f90b3d315f7a59ed866cccc51c1ec3a2d3add
Giovanni Marchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-08-08 09:56:13 +0200

    Update main.m

--- main.m
+++ main.m
@@ -5,12 +5,10 @@ close all
 img = imread('.\image.tif');
 
 C1 = cartoon2(img,10);
-C11 = addEdge(C1);
-figure(),imshow([C1,C11]),title("Cartoonization first mode");
+figure(),imshow(C1),title("Cartoonization first mode");
 
 C2 = cartoon3(img,10);
-C22 = addEdge(C2);
-figure(),imshow([C2,C22]),title("Cartoonization second mode");
+figure(),imshow(C2),title("Cartoonization second mode");
 
 NC = cartoon(double(img)/255);
 figure(),imshow(NC),title("Matlab cartoonization");
